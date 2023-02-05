f8255e76e13edc51d1d60421628aa011ad7f20e9
pangeon <kamil.cecherz@gmail.com>
2023-02-05 12:57:27 +0100

    small fix in 1.0.6

--- 1.0.6/calculator.ts
+++ 1.0.6/calculator.ts
@@ -31,7 +31,7 @@ const mod = function(first: number, second: number) {
 	return first % second;
 }
 
-const pow = function(first: number, second: number) {
+const power = function(first: number, second: number) {
 	return first ** second;
 }
 
@@ -40,11 +40,19 @@ operations.push([Operator.SUBS, subs]);
 operations.push([Operator.MULT, mult]);
 operations.push([Operator.DIV, div]);
 operations.push([Operator.MOD, mod]);
-operations.push([Operator.POW, pow]);
+operations.push([Operator.POW, power]);
 
 const calc = function(first: number, second: number, op: Operator) {
 	const tuple = operations.find(tpl => tpl[0] === op);
+	
+	// TODO: Repair it at the future !
+	// @ts-ignore - probably bug in vs code 
+	// working with command: "tsc 1.0.6/calculator.ts --target es6"
 	const operation = tuple[1];
+
+	// ! error TS2550: Property 'find' does not exist on type '[Operator, Operation][]'. 
+	// ! Do you need to change your target library? Try changing the 'lib' compiler option to 'es2015' or later.
+	
 	const result = operation(first, second);
 	return result
 }
@@ -54,4 +62,4 @@ console.log("subs", calc(13, 3, Operator.SUBS));
 console.log("mult", calc(2, 5, Operator.MULT));
 console.log("div", calc(70, 7, Operator.DIV));
 console.log("mod", calc(70, 8, Operator.MOD));
-console.log("pow", calc(2, 10, Operator.POW));
+console.log("power", calc(2, 10, Operator.POW));
