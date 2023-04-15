ac50f04c1425d2625b49be6ca3c4643c759e14df
pangeon <kamil.cecherz@gmail.com>
2023-04-15 14:51:35 +0200

    fix ex. 12.0.4 and all project dependencies problem

--- CHAP-12/12.0.4/loader.ts
+++ CHAP-12/12.0.4/loader.ts
@@ -3,6 +3,6 @@ const refresh = (message: any): void => {
     if (webContainer) webContainer.innerText = `Yours last voice: ${message}`
 }
 
-fetch('http://127.0.0.1/data.json').then((response) => {
+fetch('http://127.0.0.1:8080/CHAP-12/12.0.4/data.json').then((response) => {
   response.json().then((body) => refresh(body.message));
 });
